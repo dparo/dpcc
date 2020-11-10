f47f783e39b417db7de86adcd9b660d52012373b
Davide Paro <paro.dav.95@gmail.com>
2020-11-11 00:26:35 +0100

    Added cmocks as the unit testing framework.

--- tests/scanner_test.c
+++ tests/scanner_test.c
@@ -1,7 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <stdint.h>
+#include <cmocka.h>
 
-int main(int argc, char **argv)
-{
-    return 0;
+/* A test case that does nothing and succeeds. */
+static void null_test_success(void **state) {
+    (void) state; /* unused */
+}
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(null_test_success),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
 }
