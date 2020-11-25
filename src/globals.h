8cfc0bcb9f9ac276ca3aa444328d263403a88d76
Davide Paro <paro.dav.95@gmail.com>
2020-11-25 14:24:44 +0100

    Forgot this file in previous commit.

--- src/globals.h
+++ src/globals.h
@@ -10,4 +10,7 @@ extern mctx_t G_allctx;
 extern token_seq_t G_tok_seq;
 extern ast_node_t G_root_node;
 
+extern int32_t G_codegen_addr_inc;
+extern int32_t G_codegen_jmp_inc;
+
 void clear_all_global_vars(void);
