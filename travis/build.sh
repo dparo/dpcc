aa30d29a608df211df9d3051e6aea38f2093fb83
Davide Paro <paro.dav.95@gmail.com>
2020-12-09 13:37:42 +0100

    Code coverage setup

---
+++ travis/build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# set -euo pipefail
+
+mkdir -p travis_build && cd travis_build
+
+cmake -DCODE_COVERAGE=ON -DCMAKE_BUILD_TYPE=Debug ..
+cmake --build . --config Debug -- -j $(nproc)
+ctest -j $(nproc) --output-on-failure
